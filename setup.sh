415c20a26762832ce893da5b97cc4f78d700f0dd
willlam98 <lamcnwilliam@gmail.com>
2024-04-05 11:09:15 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#set -x
+
+dir=$(dirname $(realpath "$0"))
+os=$(uname -a | cut -d ' ' -f 1)
+mini_install=""
+
+cd $dir
+
+echo "Setting up on $os..."
+if [ "$os" != "Darwin" ] && [ "$os" != "Linux" ]
+then
+	echo "Setup for $os is not supported"
+	exit 1
+fi
+
+if [[ "$@" =~ "mini" ]]
+then
+	mini_install="mini"
+fi
+
+function setup_vimrc() {
+	mini_ver=$1
+	if [ $mini_ver == "mini" ]
+	then
+		mkdir -p ~/.vim/undo-dir
+		mkdir -p ~/.vim/plugin/
+		cp ./vim_plugin/auto-pairs.vim ~/.vim/plugin
+		cp ./.vimrc.mini ~/
+		echo "source ~/.vimrc.mini" >> ~/.vimrc
+	else
+		cp ./.vimrc.local ~/
+		echo "source ~/.vimrc.local" >> ~/.vimrc
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		vim +PlugInstall +qall
+	fi
+}
+
+function get_tools() {
+	echo "Downloading tools..."
+	if [ $os=="Darwin" ]
+	then
+		brew install gdb tig tmux tree vim wget watch grep htop ctags fzf
+	elif [ $os=="Linux" ]
+	then
+		apt update
+		apt install build-essential tmux tree tig htop vim universal-ctag \
+			silversearcher-ag fzf
+	else
+		exit 1
+	fi
+}
+
+function setup_others() {
+	cp ./.tmux.conf ~/
+	cp ./.ctags ~/
+}
+
+get_tools
+setup_vimrc $mini_install
+setup_others
